d967ef31da7141dd6508453833f916b1efe34ffd
Marushy <mersiha77@gmail.com>
2021-01-26 19:33:00 +0100

    change favicon

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
 import {
@@ -6,6 +6,7 @@ import {
   selectCartTotal,
 } from "../../redux/cart/cart.selectors";
 import CheckoutItem from "../../components/checkout-item/checkout-item.component";
+import StripeCheckoutButton from "../../components/stripe-button/stripe-button.component";
 import "./checkout.styles.scss";
 
 const CheckoutPage = ({ cartItems, total }) => {
@@ -34,6 +35,12 @@ const CheckoutPage = ({ cartItems, total }) => {
       <div className="total">
         <span>TOTAL: ${total}</span>
       </div>
+      <div className="test-warning">
+        *Please use the folloving test credit card for payment*
+        <br />
+        4242 4242 4242 4242 - Exp: 01/20 -CVV: 123
+      </div>
+      <StripeCheckoutButton price={total} />
     </div>
   );
 };
